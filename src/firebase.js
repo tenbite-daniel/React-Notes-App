0cacf1ebb401b8c4988cd38ff326fbb0e18a9a77
tenbite-daniel <tenbitedaniel60@gmail.com>
2025-07-10 01:39:09 +0300

    modified the delete functionality to delete from firebase.

--- src/firebase.js
+++ src/firebase.js
@@ -16,5 +16,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const db = getFirestore(app);
 export const notesCollection = collection(db, "notes");
